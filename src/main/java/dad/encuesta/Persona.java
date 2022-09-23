8ccefa56cd1760bc58b019f0169d26abfce2296a
KarimElKharrat <113608883+KarimElKharrat@users.noreply.github.com>
2022-09-23 10:15:58 +0100

    añadida la verificacion de nombre y apellidos

--- src/main/java/dad/encuesta/Persona.java
+++ src/main/java/dad/encuesta/Persona.java
@@ -7,9 +7,23 @@ public class Persona {
 	public Persona() {}
 	
 	public Persona(String nombre, String apellidos, int edad) {
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.edad = edad;
+		if(nombreValido(nombre) && nombreValido(apellidos)) {
+			this.nombre = nombre;
+			this.apellidos = apellidos;
+			this.edad = edad;
+		} else {
+			throw new RuntimeException("nombre o apellidos invalido");
+		}
+	}
+	
+	public boolean nombreValido(String str) {
+		int i, result = 0;
+		for(i = 0; i < str.length(); i++) {
+			if(!Character.isLetter(str.charAt(i)) && str.charAt(i) != ' ') {
+				result++;
+			}
+		}
+		return result == 0;
 	}
 	
 	@Override
